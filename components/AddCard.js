436ca80ca80b574d2260018f65e0a83833da2a87
Andres Villavicencio <villadosa@hotmail.com>
2021-03-31 16:37:54 -0500

    Create AddCard component

---
+++ components/AddCard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { View, StyleSheet} from 'react-native'
+import TextInput from '../elements/TextInput'
+import Button from '../elements/Button'
+
+export default class AddCard extends Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+  onChangeTextQuestion = (text) => {
+    this.setState({question: text})
+  }
+  onChangeTextAnswer = (text) => {
+    this.setState({answer: text})
+  }
+
+  render() {
+    const {question, answer} = this.state
+
+    return (
+      <View style={styles.container}>
+        <TextInput 
+          placeholder = 'Question'
+          onChangeText ={this.onChangeTextQuestion}
+          value = {question}
+        />
+        <TextInput 
+          placeholder = 'Answer'
+          onChangeText ={this.onChangeTextAnswer}
+          value = {answer}
+        />
+        <Button 
+          onPress={this.addCard} 
+          Name='Add Card' 
+        />
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding:50,
+    justifyContent: 'center',
+  },
+});
