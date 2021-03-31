c464d78d19ab612ad6ade2538edb996c2eb0b1e3
Andres Villavicencio <villadosa@hotmail.com>
2021-03-31 07:22:57 -0500

    Create deck component

--- components/CreateDeck.js
+++ components/CreateDeck.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { white, purple } from '../utils/colors'
 
-function mapStateToProps(state) {
-  return {
-
-  };
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+      onPress={onPress}>
+        <Text style={styles.submitBtnText}>Submit</Text>
+    </TouchableOpacity>
+  )
 }
 
-class CreateDeck extends Component {
+export default class CreateDeck extends Component {
+  state = {
+    title: ''
+  }
+
+  submit = (e) => {
+    const {title} = this.state
+    const {navigation} = this.props
+    navigation.goBack()
+  }
+
   render() {
+    const { title } = this.state
     return (
-      <View style={{flex:1}}>
-        <Text>
-          Create Deck
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.title}>
+            What is the title of your new deck?
+          </Text>
+        </View>
+        <View style={styles.box}>
+          <TextInput
+            style={styles.input}
+            value={title}
+            onChangeText={(text) => this.setState({title: text})}
+          />
+        </View> 
+        <View style={styles.box}>
+          <SubmitBtn onPress={this.submit} />
+        </View>
       </View>
     );
   }
 }
 
-export default connect(
-  mapStateToProps,
-)(CreateDeck);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding:50,
+    justifyContent: 'center',
+  },
+  box: {
+    marginBottom: 35,
+    alignContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 32
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: purple,
+    backgroundColor: white,
+    padding:10,
+    borderRadius: 5,
+    fontSize: 15,
+    height: 50,
+    marginBottom: 20
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+    color: white,
+    fontSize: 40,
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+});
