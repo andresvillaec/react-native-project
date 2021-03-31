682a02f80574851456124d901d6e236b79097123
Andres Villavicencio <villadosa@hotmail.com>
2021-03-31 11:26:56 -0500

    Create Navigation component

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Platform} from 'react-native';
-import CreateDeck from './components/CreateDeck'
-import DeckList from './components/DeckList'
-import DeckItem from './components/DeckItem'
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import SafeAreaView from 'react-native-safe-area-view';
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
-import { white, purple } from './utils/colors'
-import Constants from 'expo-constants'
+import { View} from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import middleware from './middleware'
 import reducer from './reducers'
+import Navigation from './components/Navigation'
+import { purple } from './utils/colors'
+import Constants from 'expo-constants'
 
 function CustomStatusBar ({backgroundColor, ...props}){
   return (
@@ -25,114 +16,16 @@ function CustomStatusBar ({backgroundColor, ...props}){
     </View>
   )
 }
-// Config for TabNav
-const RouteConfigs = {
-  DeckList:{
-    name: "DeckList",
-    component: DeckList,
-    options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={purple} />, title: 'Decks'}
-  }, 
-  CreateDeck:{
-    component: CreateDeck,
-    name: "Add Deck",
-    options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={purple} />, title: 'Add Deck'}
-  },
-}
-
-const TabNavigatorConfig = {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === "ios" ? purple : white,
-    style: {
-      height: 80,
-      backgroundColor: Platform.OS === "ios" ? white : purple,
-      shadowColor: "rgba(0, 0, 0, 0.24)",
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-};
-
-const Tab = Platform.OS === 'ios'
-        ? createBottomTabNavigator() 
-        : createMaterialTopTabNavigator()
-
-const TabNav = () =>(
-  <Tab.Navigator {...TabNavigatorConfig}>
-    <Tab.Screen {...RouteConfigs['DeckList']} />
-    <Tab.Screen {...RouteConfigs['CreateDeck']} />
-  </Tab.Navigator>
-)
-
-// Config for StackNav
-const StackNavigatorConfig = {
-  headerMode: "screen"
-}
-const StackConfig = {
-  TabNav:{
-    name: "Home",
-    component: TabNav,
-    options: {
-      title: 'Decks',
-      headerShown: false,
-    }
-  }, 
-  DeckItem:{
-    name: "DeckItem",
-    component: DeckItem,
-    options: {
-      title: 'Deck Detail',
-      headerTintColor: white,
-      headerStyle:{
-        backgroundColor: purple
-      }
-    }
-  },
-  CreateDeck:{
-    name: "CreateDeck",
-    component: CreateDeck,
-    options: {
-      headerTintColor: white,
-      headerStyle:{
-        backgroundColor: purple
-      }
-    }
-  }
-}
-const Stack = createStackNavigator();
-const MainNav = () =>(
-  <Stack.Navigator {...StackNavigatorConfig}>
-    <Stack.Screen {...StackConfig['TabNav']} />
-    <Stack.Screen {...StackConfig['DeckItem']} />
-    <Stack.Screen {...StackConfig['CreateDeck']} />
-  </Stack.Navigator>
-)
 
 export default class App extends React.Component{
+
   render(){
     const store = createStore(reducer, middleware)
     return(
       <Provider store={store}>
         <CustomStatusBar backgroundColor={purple} barStyle='light-content' />
-        <NavigationContainer >
-          <MainNav />
-        </NavigationContainer>
+        <Navigation/>
       </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: purple,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Navigation.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import CreateDeck from './CreateDeck'
+import DeckList from './DeckList'
+import DeckItem from './DeckItem'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { white, purple } from '../utils/colors'
+import Constants from 'expo-constants'
+
+// Config for TabNav
+const RouteConfigs = {
+  DeckList:{
+    name: "DeckList",
+    component: DeckList,
+    options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={purple} />, title: 'Decks'}
+  }, 
+  CreateDeck:{
+    component: CreateDeck,
+    name: "Add Deck",
+    options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={purple} />, title: 'Add Deck'}
+  },
+}
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 80,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+};
+
+const Tab = Platform.OS === 'ios'
+        ? createBottomTabNavigator() 
+        : createMaterialTopTabNavigator()
+
+const TabNav = () =>(
+  <Tab.Navigator {...TabNavigatorConfig}>
+    <Tab.Screen {...RouteConfigs['DeckList']} />
+    <Tab.Screen {...RouteConfigs['CreateDeck']} />
+  </Tab.Navigator>
+)
+
+// Config for StackNav
+const StackNavigatorConfig = {
+  headerMode: "screen"
+}
+const StackConfig = {
+  TabNav:{
+    name: "Home",
+    component: TabNav,
+    options: {
+      title: 'Decks',
+      headerShown: false,
+    }
+  }, 
+  DeckItem:{
+    name: "DeckItem",
+    component: DeckItem,
+    options: {
+      title: 'Deck Detail',
+      headerTintColor: white,
+      headerStyle:{
+        backgroundColor: purple
+      }
+    }
+  },
+  CreateDeck:{
+    name: "CreateDeck",
+    component: CreateDeck,
+    options: {
+      headerTintColor: white,
+      headerStyle:{
+        backgroundColor: purple
+      }
+    }
+  }
+}
+const Stack = createStackNavigator();
+const MainNav = () =>(
+  <Stack.Navigator {...StackNavigatorConfig}>
+    <Stack.Screen {...StackConfig['TabNav']} />
+    <Stack.Screen {...StackConfig['DeckItem']} />
+    <Stack.Screen {...StackConfig['CreateDeck']} />
+  </Stack.Navigator>
+)
+
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <NavigationContainer >
+        <MainNav />
+      </NavigationContainer>
+    )
+  }
+}
