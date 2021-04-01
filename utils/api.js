1266ae283457a1d8fafa54af31241709adbc77ae
Andres Villavicencio <villadosa@hotmail.com>
2021-03-31 23:57:44 -0500

    Remove useless code

--- utils/api.js
+++ utils/api.js
@@ -1,40 +1,5 @@
 import { AsyncStorage } from 'react-native';
 
-const data = {
-    React: {
-        title: 'React',
-        questions: [
-            {
-                question: 'What is React?',
-                answer: 'A library for managing user interfaces'
-            },
-            {
-                question: 'Where do you make Ajax requests in React?',
-                answer: 'The componentDidMount lifecycle event'
-            }
-        ]
-    },
-    JavaScript: {
-        title: 'JavaScript',
-        questions: [
-            {
-                question: 'What is a closure?',
-                answer: 'The combination of a function and the lexical environment within which that function was declared.'
-            }
-        ]
-    }
-};
-
-export function loadInitialDecks() {
-    try {
-        Object.keys(data).forEach((key, index) => {
-            AsyncStorage.setItem(key, JSON.stringify(data[key]));
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 export function getDecks() {
     return AsyncStorage.getAllKeys().then(keys => {
         return AsyncStorage.multiGet(keys).then(stores => {
@@ -43,7 +8,6 @@ export function getDecks() {
                 let value = JSON.parse(store[i][1]);
                 if (value) {
                     return {
-                        key,
                         title: value.title,
                         questions: value.questions
                     };
